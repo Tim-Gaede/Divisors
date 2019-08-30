e10ba57a45be1aef39efb5de65886e5406c21d91
Tim-Gaede <TimGaede@gmail.com>
2019-08-29 23:11:32 -0400

    Update function divisors(number).jl

--- function divisors(number).jl
+++ function divisors(number).jl
@@ -101,41 +101,27 @@ end
 #-------------------------------------------------------------------------------
 
 #───────────────────────────────────────────────────────────────────────────────
-function primesUpTo(limit::Integer)
-    if limit < 2;    return []; end
+function primesUpTo(lim::Integer)
+    if lim < 2;    return []; end
     primes = Int64[2]
+    sizehint!(primes, convert( Int64, floor( lim / log(lim) ) ))
+    oddsAlive = trues((lim-1) ÷ 2) # oddsAlive[i] represents 2i + 1
 
-    # To possibly save time, use the prime counting function
-    # to set the amount of memory to allocate.
-    sizehint!(primes, convert( Int64, floor( limit / log(limit) ) ))
-
-    # Keep track of all odd numbers greater than 1
-    # that are still candidates for being prime.
-    # oddsCandidates[index] represents the number, 2index + 1
-    oddsCandidates = trues((limit-1) ÷ 2)
-
-    # Iterate through the odd candidates up to the square root of the limit.
-    # If that number is still a viable candidate then it must be prime
-    # and its multiples must be eliminated as being prime.
-    i_max = (convert( Int64, floor(√limit) ) - 1) ÷ 2
-    for i = 1 : i_max
-        if oddsCandidates[i]
-            # The jumps in the index number must be an odd multiple
-            # of the index of the newly discovered prime
-            o = 2i + 1
-            for j = i+o : o : length(oddsCandidates)
-                oddsCandidates[j] = false
-            end
+    i_sqrt = (convert( Int64, floor(√lim) ) - 1) ÷ 2
+    for i = 1 : i_sqrt
+        if oddsAlive[i] # It's prime.  Kill odd multiples of it
+            push!(primes, 2i + 1)
+            Δᵢ = 2i + 1
+            for iₓ = i+Δᵢ : Δᵢ : length(oddsAlive);   oddsAlive[iₓ] = false; end
         end
     end
-
-    # Any odd number now that is still a candidate is prime.
-    for i = 1 : length(oddsCandidates)
-        if oddsCandidates[i];    push!(primes, 2i + 1); end
+    for i = i_sqrt + 1 : length(oddsAlive) # Remaining living odds also prime
+        if oddsAlive[i];    push!(primes, 2i + 1); end
     end
 
     primes
 end
+
 #───────────────────────────────────────────────────────────────────────────────
 
 #-------------------------------------------------------------------------------
@@ -186,4 +172,4 @@ function main()
     println("\n"^3, "Done!")
 end
 #═══════════════════════════════════════════════════════════════════════════════
-main()
+main() 
